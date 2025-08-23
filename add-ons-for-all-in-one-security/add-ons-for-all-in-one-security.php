6cc551ed7b273a1cf06bd2968063f133bff5d043
Vincent Bitter <5149862+vincentbitter@users.noreply.github.com>
2025-08-23 15:08:38 +0200

    Add plugins

---
+++ add-ons-for-all-in-one-security/add-ons-for-all-in-one-security.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Plugin Name: Add-ons for All-In-One Security (AIOS)
+ * Plugin URI: https://github.com/vincentbitter/add-ons-for-all-in-one-security
+ * Description: Extend AIOS with additional features.
+ * Version: 0.0.0
+ * Requires at least: 5.0
+ * Requires PHP: 5.6
+ * Author: Vincent Bitter
+ * Author URI: https://vincentbitter.nl
+ * License: GPLv3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: add-ons-for-all-in-one-security
+ * Domain Path: /languages
+ * Requires Plugins: all-in-one-wp-security-and-firewall
+ */
+
+add_filter('simba_tfa_after_user_roles', function ($output) {
+    $simba_tfa = $GLOBALS['simba_two_factor_authentication'];
+
+    $tfa_requireafter = get_option("tfa_requireafter");
+    if (!$tfa_requireafter)
+        $tfa_requireafter = 30;
+    $tfa_trusted_for = get_option("tfa_trusted_for");
+    if (!$tfa_trusted_for)
+        $tfa_trusted_for = 30;
+    ob_start();
+?>
+
+    <form method="post" action="options.php" style="margin-top: 12px">
+        <?php settings_fields('tfa_user_roles_required_group'); ?>
+        <p>
+            <?php $simba_tfa->list_user_roles_checkboxes('required_'); ?>
+        </p>
+        <p>
+            <input type="text" id="tfa_requireafter" class="tfa_requireafter" name="tfa_requireafter" size="6" style="height" value="<?php echo $tfa_requireafter  ?>">
+            <?php sprintf(__('Require enabling two-factor authentication after %s days', 'add-ons-for-all-in-one-security'), '<input type="text" id="tfa_requireafter" class="tfa_requireafter" name="tfa_requireafter" size="6" style="height">'); ?>
+        </p>
+        <p>
+            <input type="text" id="tfa_trusted_for" class="tfa_trusted_for" name="tfa_trusted_for" size="6" style="height" value="<?php echo $tfa_trusted_for  ?>">
+            <?php sprintf(__('Remember two-factor authentiation for %s days', 'add-ons-for-all-in-one-security'), '<input type="text" id="tfa_trusted_for" class="tfa_trusted_for" name="tfa_trusted_for" size="6" style="height">'); ?>
+        </p>
+
+        <?php submit_button(); ?>
+    </form>
+
+<?php
+    $html = ob_get_contents();
+    ob_end_clean();
+
+    return $html;
+});
---
+++ add-ons-for-smash-balloon-feeds/add-ons-for-smash-balloon-feeds.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Plugin Name: Add-ons for Smash Balloon Feeds
+ * Plugin URI: https://github.com/vincentbitter/add-ons-for-all-in-one-security
+ * Description: Extend Smash Balloon plugins like Custom Facebook Feed with additional features.
+ * Version: 0.0.0
+ * Requires at least: 5.0
+ * Requires PHP: 5.6
+ * Author: Vincent Bitter
+ * Author URI: https://vincentbitter.nl
+ * License: GPLv3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+$sbfeeds_supported_plugins = [
+    'custom-facebook-feed/custom-facebook-feed.php',
+    'custom-twitter-feeds/custom-twitter-feed.php',
+];
+
+register_activation_hook(__FILE__, function () use ($sbfeeds_supported_plugins) {
+    foreach ($sbfeeds_supported_plugins as $plugin_file) {
+        $plugin_name = explode(DIRECTORY_SEPARATOR, $plugin_file)[0];
+        sbfeeds_patch($plugin_name);
+    }
+});
+
+foreach ($sbfeeds_supported_plugins as $plugin_file) {
+    register_activation_hook($plugin_file, function () use ($plugin_file) {
+        $plugin_name = explode(DIRECTORY_SEPARATOR, $plugin_file)[0];
+        sbfeeds_patch($plugin_name);
+    });
+}
+
+add_action('upgrader_process_complete', function ($upgrader, $options) {
+    if ($options['type'] !== 'plugin' || empty($options['plugins'])) return;
+
+    foreach ($options['plugins'] as $pluginPath) {
+        $plugin_name = explode(DIRECTORY_SEPARATOR, $pluginPath)[0];
+        sbfeeds_patch($plugin_name);
+    }
+}, 10, 2);
+
+function sbfeeds_patch($plugin_name)
+{
+    $source_dir = plugin_dir_path(__FILE__) . $plugin_name . DIRECTORY_SEPARATOR;
+    $dest_dir = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin_name . DIRECTORY_SEPARATOR;
+
+    if (is_dir($source_dir) && is_dir($dest_dir)) {
+        sbfeeds_patch_dir($source_dir, $dest_dir);
+    }
+}
+
+function sbfeeds_patch_dir($source, $destination)
+{
+    if (!is_dir($source) || !is_dir($destination)) return;
+
+    $dir = opendir($source);
+    @mkdir($destination, 0755, true);
+
+    while (($file = readdir($dir)) !== false) {
+        if ($file === '.' || $file === '..') continue;
+
+        $srcFile = $source . $file;
+        $destFile = $destination . $file;
+
+        if (is_dir($srcFile)) {
+            sbfeeds_patch_dir($srcFile . DIRECTORY_SEPARATOR, $destFile . DIRECTORY_SEPARATOR);
+        } else {
+            if (str_ends_with($srcFile, '.patch'))
+                sbfeeds_patch_file($srcFile, substr($destFile, 0, -6));
+            else
+                copy($srcFile, $destFile);
+        }
+    }
+
+    closedir($dir);
+}
+
+function sbfeeds_patch_file($patchFile, $targetFile)
+{
+    copy($targetFile, $targetFile . '.bak');
+
+    $patch = json_decode(file_get_contents($patchFile), true);
+    if (json_last_error() !== JSON_ERROR_NONE) return;
+
+    $content = file_get_contents($targetFile);
+    foreach ($patch as $change) {
+        if ($change['op'] === 'replace') {
+            $content = str_replace($change['find'], $change['value'], $content);
+        }
+    }
+
+    file_put_contents($targetFile, $content);
+}
